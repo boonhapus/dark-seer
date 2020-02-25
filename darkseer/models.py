032b1193f7f990a63b6ac7e02c05cce75367733a
SupahNoob <importantspamacct@gmail.com>
2020-02-25 16:26:01 -0600

    minor updates to models

--- darkseer/models.py
+++ darkseer/models.py
@@ -5,15 +5,26 @@ from darkseer.database import Base
 
 # elements which exist w.r.t the game of dota
 
+class GameVersion(Base):
+    __tablename__ = 'game_version'
+    patch_id = Column(Integer, primary_key=True)
+    patch = Column(String)
+    release_datetime = Column(DateTime, comment='held as naive, but UTC')
+
 class Hero(Base):
     __tablename__ = 'hero'
+    hero_id = Column(Integer, primary_key=True, autoincrement=False)
+    ...
 
 class Ability(Base):
     __tablename__ = 'ability'
+    ability_id = Column(Integer, primary_key=True, autoincrement=False)
+    ...
 
 class Item(Base):
     __tablename__ = 'item'
-
+    item_id = Column(Integer, primary_key=True, autoincrement=False)
+    ...
 
 # elements which exist w.r.t. a Match of dota
 
