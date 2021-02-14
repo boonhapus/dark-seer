e9d0c5d81ab7090db67bd145ce9761fae1a67d2d
Nick Cooper <cooper.b.nicholas@gmail.com>
2021-02-14 01:31:36 -0600

    support async databases

--- darkseer/database.py
+++ darkseer/database.py
@@ -3,6 +3,7 @@ import logging
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
+import databases
 import sqlalchemy as sa
 
 
@@ -11,32 +12,34 @@ log = logging.getLogger(__name__)
 
 
 class Database:
-    def __init__(self, conn_string: str):
-        self._engine = sa.create_engine(conn_string)
-        self._session_factory = sessionmaker(bind=self._engine)
-        self._Session = scoped_session(self._session_factory)
+    def __init__(self, conn_string: str, echo: bool=False):
+        self._db = databases.Database(conn_string)
+        self._engine = sa.create_engine(conn_string, echo=echo)
+        # self._session_factory = sessionmaker(bind=self._engine)
+        # self._Session = scoped_session(self._session_factory)
+        self._metadata = Base.metadata
 
     @property
     def engine(self):
         return self._engine
 
-    @contextmanager
-    def session(self, **kwargs) -> Session:
-        """
-        Handles all the messy details of session work.
-        """
-        self._session = sess = self._Session(**kwargs)
+    # @contextmanager
+    # def session(self, **kwargs) -> Session:
+    #     """
+    #     Handles all the messy details of session work.
+    #     """
+    #     self._session = sess = self._Session(**kwargs)
 
-        try:
-            yield sess
-            sess.commit()
-        except Exception as e:
-            sess.rollback()
-            log.exception(f'{type(e).__name__}: {e}')
-            # raise e
-        finally:
-            sess.close()
-            self._session = None
+    #     try:
+    #         yield sess
+    #         sess.commit()
+    #     except Exception as e:
+    #         sess.rollback()
+    #         log.exception(f'{type(e).__name__}: {e}')
+    #         # raise e
+    #     finally:
+    #         sess.close()
+    #         self._session = None
 
     def __repr__(self):
         return f'<Database {self._engine.url}>'
