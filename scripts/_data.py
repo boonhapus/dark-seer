a5703ecb6a714f2cab87d2d99975d22364de510f
boonhapus <me@boonhap.us>
2021-05-30 15:50:32 -0500

    💻 add cli command to fill patch data

--- scripts/_data.py
+++ scripts/_data.py
@@ -1,6 +1,44 @@
+import pathlib
+
+from darkseer.informants import Stratz
+from darkseer.database import Database
+from darkseer.models import GameVersion
+from darkseer.util import upsert
+from typer import Argument as A_, Option as O_
 import typer
 
+from .common import to_csv
+from ._async import _coro
+from ._db import db_options
+from ._ux import console, RichGroup, RichCommand
 
-app = typer.Typer(
-    help='Perform data functions.'
-)
+
+app = typer.Typer(help='Perform data functions.', cls=RichGroup)
+
+stratz_app = typer.Typer(help='Collect data from Stratz.', cls=RichGroup)
+app.add_typer(stratz_app, name='stratz')
+
+
+@stratz_app.command(cls=RichCommand)
+@db_options
+@_coro
+async def patch(
+    patch: str=O_(None, help='Specific patch to get data for.'),
+    save_path: pathlib.Path=O_(None, help='Directory to save data pull to.'),
+    token: str=O_(None, help='STRATZ Bearer token for elevated requests permission'),
+    **db_options
+):
+    """
+    Connect an interactive session to the database.
+    """
+    db = Database(**db_options)
+
+    async with Stratz(bearer_token=token) as api:
+        r = await api.patches()
+
+    if save_path is not None:
+        to_csv(save_path / 'patches.csv', data=[v.dict() for v in r])
+
+    async with db.session() as sess:
+        stmt = upsert(GameVersion).values([v.dict() for v in r])
+        await sess.execute(stmt)
